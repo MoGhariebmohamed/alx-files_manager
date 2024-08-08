af11e16666b343a56fe6a61af40c7bbd3e6b3ae0
Mo Gharieb <m.gharieb89@gmail.com>
2024-08-08 04:57:42 +0300

    t3v2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
 //import AuthController from '../controllers/AuthController';
-//import UsersController from '../controllers/UsersController';
+import UsersController from '../controllers/UsersController';
 //import FilesController from '../controllers/FilesController';
 //import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 //import { APIError, errorResponse } from '../middlewares/error';
