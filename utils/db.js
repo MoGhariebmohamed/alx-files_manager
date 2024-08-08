9b396f45f82d084a2325dc128f6846347790953b
Mo Gharieb <m.gharieb89@gmail.com>
2024-08-08 04:25:40 +0300

    task1 rev3

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,31 @@
 import mongodb from 'mongodb';
 import Collection from 'mongodb/lib/collection';
-import envLoader from './env_loader';
+import { existsSync, readFileSync } from 'fs';
 
 /**
  * Represents a MongoDB
  */
+
+
+/**
+ * Loads the appropriate environment variables for an event.
+ */
+const envLoader = () => {
+  const env = process.env.npm_lifecycle_event || 'dev';
+  const path = env.includes('test') || env.includes('cover') ? '.env.test' : '.env';
+
+  if (existsSync(path)) {
+    const data = readFileSync(path, 'utf-8').trim().split('\n');
+
+    for (const line of data) {
+      const delimPosition = line.indexOf('=');
+      const variable = line.substring(0, delimPosition);
+      const value = line.substring(delimPosition + 1);
+      process.env[variable] = value;
+    }
+  }
+};
+
 class DBClient {
   /**
    * Creates new DBClient instance.
@@ -43,7 +64,6 @@ class DBClient {
 
   /**
    * Retrieves a reference to the `users` collection.
-   * @returns {Promise<Collection>}
    */
   async usersCollection() {
     return this.client.db().collection('users');
